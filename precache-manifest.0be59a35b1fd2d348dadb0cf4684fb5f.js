e93014d699cf67c618cc9a1ffeb5e6141347ed60
gemtunde <gemtunde@gmail.com>
2020-08-28 20:32:34 +0100

    Updates

---
+++ precache-manifest.0be59a35b1fd2d348dadb0cf4684fb5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f197807d3bbe5bab04ecd5f2779588",
+    "url": "/ReactWeb/index.html"
+  },
+  {
+    "revision": "8ac455d30d81f92afab2",
+    "url": "/ReactWeb/static/css/main.bfd767b2.chunk.css"
+  },
+  {
+    "revision": "3531f904f783f09dfea8",
+    "url": "/ReactWeb/static/js/2.a8205785.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactWeb/static/js/2.a8205785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac455d30d81f92afab2",
+    "url": "/ReactWeb/static/js/main.884792c6.chunk.js"
+  },
+  {
+    "revision": "534e3d3fd2c4a133c444",
+    "url": "/ReactWeb/static/js/runtime-main.3bea4e35.js"
+  }
+]);
